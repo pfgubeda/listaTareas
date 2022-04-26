fc54e7b287d18bb3f32b8c97376eda53c2ebbf62
pfgubeda <pabloubeda2000@gmail.com>
2022-04-26 13:51:48 +0200

    Merge branch 'master' of https://github.com/pfgubeda/listaTareas

--- src/app/componentes/tareas-principal/tareas-principal.component.ts
+++ src/app/componentes/tareas-principal/tareas-principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-tareas-principal',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasPrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor( public authService:AuthService) { }
 
   ngOnInit(): void {
   }
